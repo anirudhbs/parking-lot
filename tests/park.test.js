001a35c61e7efbecd1a1e2d0cb450ddd83de692d
anirudhbs <anirudhbs96@gmail.com>
2019-05-07 17:54:28 +0530

    Add test for scenario where lot is full

--- tests/park.test.js
+++ tests/park.test.js
@@ -1,6 +1,9 @@
-const { park } = require('../src/methods')
+const { createParkingLot, park } = require('../src/methods')
 
 describe('createParkingLot', () => {
+  // create a parking lot with 7 slots
+  createParkingLot(7)
+
   it(`It should park the car in the nearest open slot`, () => {
     expect(park('KA-01-HH-1234', 'White')).toMatch(/Allocated slot number/)
   })
@@ -15,5 +18,15 @@ describe('createParkingLot', () => {
 
   it(`It should throw an error asking for a valid number`, () => {
     expect(park()).toBe(false)
+  })
+
+  it('It should say return a message saying all slots in the lot are occupied', () => {
+    park('KA-01-HH-1234', 'White')
+    park('KA-01-HH-2345', 'Red')
+    park('KA-01-HH-3456', 'White')
+    park('KA-01-HH-4567', 'Blue')
+    park('KA-01-HH-5678', 'White')
+    park('KA-01-HH-6789', 'Black')
+    expect(park('KA-05-JK-1921', 'Black')).toBe('All slots in the lot are occupied!')
   })
 })
