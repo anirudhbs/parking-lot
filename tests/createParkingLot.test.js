ab88e478d22ce2a15a4feb266849493dc4b90c4d
anirudhbs <anirudhbs96@gmail.com>
2019-05-07 18:30:32 +0530

    Update tests with new method signature

--- tests/createParkingLot.test.js
+++ tests/createParkingLot.test.js
@@ -1,12 +1,12 @@
-const { createParkingLot } = require('../src/methods')
+const createParkingLot = require('../src/operations/createParkingLot')
 
 describe('createParkingLot', () => {
   const n = 6
   it(`It should create a parking lot with ${n} slots`, () => {
-    expect(createParkingLot(n)).toBe(`Created a parking lot with ${n} slots`)
+    expect(createParkingLot({}, n)).toBe(`Created a parking lot with ${n} slots`)
   })
 
   it(`It should throw an error asking for a valid number`, () => {
-    expect(createParkingLot(null)).toBe('enter a valid number')
+    expect(createParkingLot({}, null)).toBe('enter a valid number')
   })
 })
--- tests/park.test.js
+++ tests/park.test.js
@@ -1,36 +1,38 @@
-const { createParkingLot, park } = require('../src/methods')
+const createParkingLot = require('../src/operations/createParkingLot')
+const park = require('../src/operations/park')
 
 describe('createParkingLot', () => {
+  const lot = {}
   // create a parking lot with 7 slots
-  createParkingLot(7)
+  createParkingLot(lot, 7)
 
   it(`It should park the car in the nearest open slot`, () => {
-    expect(park('KA-01-HH-1234', 'White')).toMatch(/Allocated slot number/)
+    expect(park(lot, 'KA-01-HH-1234', 'White')).toMatch(/Allocated slot number/)
   })
 
   it(`It should throw an error asking for a valid number`, () => {
-    expect(park('KA-01-HH-1234', 101)).toBe(
+    expect(park(lot, 'KA-01-HH-1234', 101)).toBe(
       'Invalid input, make sure you enter both the plate, and the color'
     )
   })
 
   it(`It should throw an error asking for a valid number`, () => {
-    expect(park(undefined, 'White')).toBe(
+    expect(park(lot, undefined, 'White')).toBe(
       'Invalid input, make sure you enter both the plate, and the color'
     )
   })
 
   it(`It should throw an error asking for a valid number`, () => {
-    expect(park()).toBe('Invalid input, make sure you enter both the plate, and the color')
+    expect(park(lot)).toBe('Invalid input, make sure you enter both the plate, and the color')
   })
 
   it('It should say return a message saying all slots in the lot are occupied', () => {
-    park('KA-01-HH-1234', 'White')
-    park('KA-01-HH-2345', 'Red')
-    park('KA-01-HH-3456', 'White')
-    park('KA-01-HH-4567', 'Blue')
-    park('KA-01-HH-5678', 'White')
-    park('KA-01-HH-6789', 'Black')
-    expect(park('KA-05-JK-1921', 'Black')).toBe('All slots in the lot are occupied!')
+    park(lot, 'KA-01-HH-1234', 'White')
+    park(lot, 'KA-01-HH-2345', 'Red')
+    park(lot, 'KA-01-HH-3456', 'White')
+    park(lot, 'KA-01-HH-4567', 'Blue')
+    park(lot, 'KA-01-HH-5678', 'White')
+    park(lot, 'KA-01-HH-6789', 'Black')
+    expect(park(lot, 'KA-05-JK-1921', 'Black')).toBe('All slots in the lot are occupied!')
   })
 })
