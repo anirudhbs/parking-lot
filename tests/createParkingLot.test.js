f4e2625b06aa7769240d983337a9568b1f4a0854
anirudhbs <anirudhbs96@gmail.com>
2019-05-08 14:16:58 +0530

    Add a test case for creating multiple lots

--- tests/createParkingLot.test.js
+++ tests/createParkingLot.test.js
@@ -6,6 +6,12 @@ describe('createParkingLot', () => {
     expect(createParkingLot({}, n)).toBe(`Created a parking lot with ${n} slots`)
   })
 
+  it('It should send a message saying that a lot has already been created', () => {
+    const lot = {}
+    createParkingLot(lot, n)
+    expect(createParkingLot(lot, n)).toBe('A lot was already created, cannot create another one')
+  })
+
   it(`It should send a message asking for a valid number`, () => {
     expect(createParkingLot({}, null)).toBe('enter a valid number')
   })
