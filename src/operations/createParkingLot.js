450a90d87a36a21e45cf0d02586429e1be8d7d9a
anirudhbs <anirudhbs96@gmail.com>
2019-05-08 14:18:02 +0530

    Handle case for creating multiple lots

--- src/operations/createParkingLot.js
+++ src/operations/createParkingLot.js
@@ -5,6 +5,10 @@ function createParkingLot (lot, n) {
     return 'enter a valid number'
   }
 
+  if (Object.keys(lot).length > 0) {
+    return 'A lot was already created, cannot create another one'
+  }
+
   for (let i = 1; i <= num; i += 1) {
     lot[i] = null
   }
