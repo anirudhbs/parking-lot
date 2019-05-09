ae4d626f073a15c2581a7a34c18e7f28f14b9c86
anirudhbs <anirudhbs96@gmail.com>
2019-05-09 11:22:12 +0530

    Update few more tests for new createParkingLot signature

--- tests/getLotDetails.test.js
+++ tests/getLotDetails.test.js
@@ -3,16 +3,18 @@ const createParkingLot = require('../src/operations/createParkingLot')
 const park = require('../src/operations/park')
 
 describe('autoComplete', () => {
-  let lot = {}
   const n = 4
-  createParkingLot(lot, n)
 
   it('It should return an empty lot', () => {
+    let lot = createParkingLot({}, n)[0]
+
     const expectedObj = {}
     expect(getLotDetails(lot)).toMatchObject(expectedObj)
   })
 
   it('It should return an lot details with only used slots', () => {
+    let lot = createParkingLot({}, n)[0]
+
     const expectedObj = {
       1: {
         plate: 'KA-01-HH-3456',
@@ -31,6 +33,7 @@ describe('autoComplete', () => {
     lot = park(lot, 'KA-01-HH-3456', 'White')[0]
     lot = park(lot, 'KA-01-HH-4567', 'Blue')[0]
     lot = park(lot, 'KA-01-HH-5678', 'White')[0]
+
     expect(getLotDetails(lot)).toMatchObject(expectedObj)
   })
 })
--- tests/getMatchingValues.test.js
+++ tests/getMatchingValues.test.js
@@ -2,20 +2,26 @@ const { getMatchingValues } = require('../src/utils')
 const createParkingLot = require('../src/operations/createParkingLot')
 const park = require('../src/operations/park')
 
-describe('autoComplete', () => {
-  let lot = {}
+describe('Get cars with matching value', () => {
   const n = 4
-  createParkingLot(lot, n)
-  lot = park(lot, 'KA-01-HH-3456', 'White')[0]
-  lot = park(lot, 'KA-01-HH-4567', 'Blue')[0]
-  lot = park(lot, 'KA-01-HH-5678', 'White')[0]
 
   it('It should return an empty object', () => {
+    let lot = createParkingLot({}, n)[0]
+
+    lot = park(lot, 'KA-01-HH-3456', 'White')[0]
+    lot = park(lot, 'KA-01-HH-4567', 'Blue')[0]
+
     const expectedObj = {}
     expect(getMatchingValues(lot, value => value.color === 'Pink')).toMatchObject(expectedObj)
   })
 
   it('It should return object with cars which pass the predicate', () => {
+    let lot = createParkingLot({}, n)[0]
+
+    lot = park(lot, 'KA-01-HH-3456', 'White')[0]
+    lot = park(lot, 'KA-01-HH-4567', 'Blue')[0]
+    lot = park(lot, 'KA-01-HH-5678', 'White')[0]
+
     const expectedObj = {
       1: {
         plate: 'KA-01-HH-3456',
