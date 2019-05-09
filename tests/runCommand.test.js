d3b47aa7af46161263df7f32ed8ae457225e5596
anirudhbs <anirudhbs96@gmail.com>
2019-05-09 11:41:31 +0530

    Add test cases for wrong input for runCommand method

--- tests/runCommand.test.js
+++ tests/runCommand.test.js
@@ -8,4 +8,16 @@ describe('runCommand', () => {
   it('It should return a message saying that a lot has been created', () => {
     expect(runCommand(['create_parking_lot', '6'])).toBe('Created a parking lot with 6 slots')
   })
+
+  it('It should throw an error for wrong params data type', () => {
+    expect(() => {
+      runCommand(null)
+    }).toThrow(TypeError)
+  })
+
+  it('It should throw an error for wrong params data type', () => {
+    expect(() => {
+      runCommand(1234)
+    }).toThrow(TypeError)
+  })
 })
