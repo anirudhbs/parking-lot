6ea5193d867dfd88a83f0e15659715dad4f3347c
anirudhbs <anirudhbs96@gmail.com>
2019-05-09 12:37:08 +0530

    Rename num -> slot in park method

--- src/operations/park.js
+++ src/operations/park.js
@@ -2,24 +2,24 @@ function park (lot, plate, color) {
   if (typeof plate !== 'string' || typeof color !== 'string') {
     throw new TypeError('Invalid input, make sure you enter both the plate, and the color')
   }
-  let num = null
+  let slot = null
   const slots = Object.keys(lot)
 
   // find the first open slot, ie. first value to be null
   for (let i = 1; i <= slots.length; i += 1) {
     if (lot[i] === null) {
-      num = i
+      slot = i
       break
     }
   }
-  if (num === null) {
+  if (slot === null) {
     throw new Error('Sorry, parking lot is full')
   }
 
   const newLot = Object.assign({}, lot)
-  newLot[num] = { plate, color }
+  newLot[slot] = { plate, color }
 
-  return [newLot, num]
+  return [newLot, slot]
 }
 
 module.exports = park
